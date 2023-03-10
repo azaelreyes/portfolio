501dd3bbac2cf6ecc7fe71b49fb3fd69b5f93d6d
azaelreyes <azaelreyes20.ar@gmail.com>
2023-03-09 16:15:48 -0800

    finished Js for highlighting corresponding Nav Bar item as you scroll, also shortened and cleaned up Js for Scroll up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,5 @@
 const scrollUp = document.getElementById("scroll-up");
-const showOnScreen = 500;
-//shows Scroll Up only when not in homepage
-document.addEventListener("scroll",()=>{
-    if(document.documentElement.scrollTop < showOnScreen){
-        scrollUp.classList.add("hidden");
-    } else{
-        scrollUp.classList.remove("hidden");
-    }
-})
-//scrolls page to home page when Scroll Up is clicked
+
 scrollUp.addEventListener("click", ()=>{document.body.scrollIntoView({behavior:"smooth"});})
 
 
@@ -19,4 +10,30 @@ const contactSidebarBtn = document.getElementById("contact-sidebar-btn");
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".nav-item");
 
-console.log(navLi)
+document.addEventListener("scroll", () =>{
+    //shows and hides scroll up button, also toggles homesidebarbtn active link class
+    if(document.documentElement.scrollTop < 500 ){
+        homeSidebarBtn.classList.add("active-link")
+        projectsSidebarBtn.classList.remove("active-link")
+        contactSidebarBtn.classList.remove("active-link");
+        scrollUp.classList.add("hidden");
+    } else{
+        scrollUp.classList.remove("hidden");
+        homeSidebarBtn.classList.remove("active-link")
+
+    }
+
+    //these toggle the projects & contact sidebar btn active link class
+    if(window.pageYOffset > 500 && window.pageYOffset < 2500){
+        homeSidebarBtn.classList.remove("active-link")
+        projectsSidebarBtn.classList.add("active-link")
+        contactSidebarBtn.classList.remove("active-link");
+    } 
+    if(window.pageYOffset > 2500 && window.pageYOffset <10000){
+        projectsSidebarBtn.classList.remove("active-link")
+        homeSidebarBtn.classList.remove("active-link")
+        contactSidebarBtn.classList.add("active-link");
+    }
+    
+})
+
